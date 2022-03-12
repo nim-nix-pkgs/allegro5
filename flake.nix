734157f33c031957116dc42146973ea62e470327
hugosenari <hugosenari@gmail.com>
2022-03-12 00:18:20 -0300

    chore: re index allegro5

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''Wrapper for Allegro version 5.X'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."allegro5-master".url = "path:./master";
+  inputs."allegro5-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."allegro5-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."allegro5-v1_0".url = "path:./v1_0";
+  inputs."allegro5-v1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."allegro5-v1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Wrapper for Allegro version 5.X'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-allegro5-master.flake = false;
+  inputs.src-allegro5-master.owner = "fowlmouth";
+  inputs.src-allegro5-master.ref   = "refs/heads/master";
+  inputs.src-allegro5-master.repo  = "allegro5";
+  inputs.src-allegro5-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-allegro5-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-allegro5-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
